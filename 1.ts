8240e20eaf616ac15d7268fcff8c087f73e0789b
NamiqPlanov245 <planovnamiq27@gmail.com>
2023-02-06 12:42:16 +0400

    typescript initial

---
+++ 1.ts
@@ -0,0 +1,14 @@
+type courses = 'hello'|'goodbye'|'how are you'|'okay'
+type data = {
+    name: string,
+    course:courses,
+    age:number
+    drive_license: string
+
+}
+let info: data   = {
+    name : 'Namiq',
+    course: 'hello',
+    age: 19,
+    drive_license: 'B'
+}
